b405b9a400f7623b4bde7ca20566ef2d019ea570
fthomasvp <fellipecab@gmail.com>
2022-10-22 03:53:50 -0300

    refactor: show illustration when user's password is updated

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,13 +1,14 @@
-import { Flex, Heading, Image } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
-import { PasswordGreenLocker } from "../assets/images";
+import { PasswordGreenLocker, UpdatedStatusNeonCarrot } from "../assets/images";
 import { BackButton, HalfBall } from "../components/ui";
 import {
-  FormNewPassword,
+  FormResetPassword,
   resetPasswordSchema,
   ResetPasswordType,
 } from "../features/authentication";
@@ -21,33 +22,65 @@ export default function ResetPassword() {
     resolver: yupResolver(resetPasswordSchema(t)),
   });
 
+  const [isPassReseted, setIsPassReseted] = useState(false);
+
   const onSubmit: SubmitHandler<ResetPasswordType> = (data) => {
     console.log("> ResetPassword :: data", data);
 
-    navigate("/", { replace: true });
+    setIsPassReseted(true);
+  };
+
+  const navigateToLogin = () => {
+    navigate("/login", { replace: true });
   };
 
   return (
     <MobileContainer>
       <HalfBall right={0} />
 
-      <Flex flex={1}>
-        <BackButton />
-      </Flex>
+      {!isPassReseted ? (
+        <VStack flex={1} justify="center">
+          <Flex justifyContent="center" mb="10">
+            <Image
+              alt="person holding updated cards"
+              src={UpdatedStatusNeonCarrot}
+            />
+          </Flex>
 
-      <Flex flex={1}>
-        <Heading fontWeight="semibold">{t("enterNewPassword")}</Heading>
-      </Flex>
+          <VStack spacing="3">
+            <Text fontSize="lg" fontWeight="semibold">
+              {t("updatedPassword")}
+            </Text>
 
-      <Flex flex={1} justifyContent="center" mb="10">
-        <Image src={PasswordGreenLocker} alt="person closing a green locker" />
-      </Flex>
+            <Button variant="link" onClick={navigateToLogin}>
+              {t("doLoginToContinue", { ns: "glossary" })}
+            </Button>
+          </VStack>
+        </VStack>
+      ) : (
+        <>
+          <Flex flex={1}>
+            <BackButton />
+          </Flex>
 
-      <Flex flex={2} flexDir="column">
-        <FormProvider {...methods}>
-          <FormNewPassword onSubmit={onSubmit} />
-        </FormProvider>
-      </Flex>
+          <Flex flex={1}>
+            <Heading fontWeight="semibold">{t("enterNewPassword")}</Heading>
+          </Flex>
+
+          <Flex flex={1} justifyContent="center" mb="10">
+            <Image
+              src={PasswordGreenLocker}
+              alt="person closing a green locker"
+            />
+          </Flex>
+
+          <Flex flex={2} flexDir="column">
+            <FormProvider {...methods}>
+              <FormResetPassword onSubmit={onSubmit} />
+            </FormProvider>
+          </Flex>
+        </>
+      )}
 
       <HalfBall bottom={0} right={0} boxSize="10" />
     </MobileContainer>
