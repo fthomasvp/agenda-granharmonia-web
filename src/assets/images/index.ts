d861b6ee797bce72a174235a70c8a92f0cc903f9
fthomasvp <fellipecab@gmail.com>
2022-10-22 03:49:43 -0300

    style: add Illustration to show on ResetPassword page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,4 @@ export { default as OxentiLabLogo } from "./oxenti_lab_logo.svg";
 export { default as Ball } from "./ball.svg";
 export { default as MobileGreenCheck } from "./mobile_green_check.svg";
 export { default as PasswordGreenLocker } from "./password_green_locker.svg";
+export { default as UpdatedStatusNeonCarrot } from "./undraw_updated_status_neon_carrot.svg";
