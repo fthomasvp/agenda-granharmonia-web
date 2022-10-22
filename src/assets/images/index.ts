3e988cd74ef68b535e702ba7c0ef6a902200d228
fthomasvp <fellipecab@gmail.com>
2022-10-22 15:11:27 -0300

    refactor: remove unused illustration

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,5 +2,4 @@ export { default as GranHarmoniaLogo } from "./gran_harmonia_logo.svg";
 export { default as OxentiLabLogo } from "./oxenti_lab_logo.svg";
 export { default as Ball } from "./ball.svg";
 export { default as MobileGreenCheck } from "./mobile_green_check.svg";
-export { default as PasswordGreenLocker } from "./password_green_locker.svg";
 export { default as UpdatedStatusNeonCarrot } from "./undraw_updated_status_neon_carrot.svg";
